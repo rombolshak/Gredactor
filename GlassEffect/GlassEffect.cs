e21b60c51c172e6affef44a1a325aa866619e9be
rombolshak <rombolshak@russia.ru>
2012-09-30 13:23:55 +0400

    Glass Effect

---
+++ GlassEffect/GlassEffect.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Gredactor;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace GlassEffect
+{
+    public class GlassEffect : IEffect
+    {
+        public string Name
+        {
+            get { return "Стекло"; }
+        }
+
+        public string Description
+        {
+            get { return "Эффект стекла"; }
+        }
+
+        public bool Prepare(object obj)
+        {
+            return true;
+        }
+
+        public Bitmap Apply(Bitmap original)
+        {
+            Rectangle rect = new Rectangle(0, 0, original.Width, original.Height);
+            System.Drawing.Imaging.BitmapData bmpData = original.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            IntPtr ptr = bmpData.Scan0;
+            int bytes = Math.Abs(bmpData.Stride) * bmpData.Height;
+            byte[] values = new byte[bytes];
+            byte[] newValues = new byte[bytes];
+            System.Runtime.InteropServices.Marshal.Copy(ptr, values, 0, bytes);
+
+
+            Random r = new Random();
+            for (int y = 0; y < bmpData.Height; ++y)
+                for (int x = 0; x < bmpData.Width; ++x)
+                {
+                    int index = (y * bmpData.Width + x) * 3;
+                    if (index + 2 >= bytes) break;
+
+                    int fromX = (int)(x - 10 * (r.NextDouble() - .5));
+                    int fromY = (int)(y - 10 * (r.NextDouble() - .5));
+                    if ((fromX < 0) || (fromX >= bmpData.Width)) fromX = x;
+                    if ((fromY < 0) || (fromY >= bmpData.Height)) fromY = y;
+                    int fromIndex = (fromY * bmpData.Width + fromX) * 3;
+
+                    newValues[index + 2] = values[fromIndex + 2];
+                    newValues[index + 1] = values[fromIndex + 1];
+                    newValues[index + 0] = values[fromIndex + 0];
+                }
+
+            System.Runtime.InteropServices.Marshal.Copy(newValues, 0, ptr, bytes);
+            original.UnlockBits(bmpData);
+            return original;
+        }
+
+        public string MenuGroup
+        {
+            get { return "Фильтры"; }
+        }
+
+        public ToolStripMenuItem[] MenuItems
+        {
+            get { return new ToolStripMenuItem[] { new ToolStripMenuItem(this.Name) }; }
+        }
+
+        public Button[] Buttons
+        {
+            get { Button b = new Button(); b.Text = this.Name; return new Button[] { b }; }
+        }
+
+        public char[] ShortConsoleKey
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string[] LongConsoleKey
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
